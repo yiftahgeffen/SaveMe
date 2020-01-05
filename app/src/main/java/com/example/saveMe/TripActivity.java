74cd0c5ba0092f7b247f5f10c90d541b7af2110b
yiftah geffen <yiftah.geffen@carlsonwagonlit.com>
2020-01-05 21:13:14 +0200

    NEW APP

--- app/src/main/java/com/example/saveMe/TripActivity.java
+++ app/src/main/java/com/example/saveMe/TripActivity.java
@@ -1,16 +1,56 @@
 package com.example.saveMe;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 import com.example.hackathon.R;
-
 public class TripActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
     }
+    public void openMenuBar(View view) {
+
+        Intent intent = new Intent(this, TripActivity.class);
+        PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu, popup.getMenu());
+        popup.show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        System.out.println("helle_2");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        System.out.println("helle_3");
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.wearable_devices:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+//    public void openWearableActivity(View view) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//    }
+
 }
