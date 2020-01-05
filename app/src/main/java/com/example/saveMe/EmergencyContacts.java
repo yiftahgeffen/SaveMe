660b921208c75a632622b08e0b90720b776c94da
yiftah geffen <yiftah.geffen@carlsonwagonlit.com>
2020-01-05 22:38:26 +0200

    Merge branch 'master' of github.com:yiftahgeffen/SaveMe
    # Conflicts:
    #	app/src/main/java/com/example/saveMe/TripActivity.java
    #	app/src/main/res/menu/menu.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/saveMe/EmergencyContacts.java
@@ -0,0 +1,16 @@
+package com.example.saveMe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hackathon.R;
+
+public class EmergencyContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_contacts);
+    }
+}
